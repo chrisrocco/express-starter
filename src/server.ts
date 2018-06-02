aa3fff3eef248f529b723e5c8cc1fe67e1849477
Chris Rocco <chris.rocco7@gmail.com>
2018-06-02 12:30:34 -0500

    added retry wrapper

--- src/server.ts
+++ src/server.ts
@@ -1,29 +1,19 @@
 import {getApp} from "./app"
 import {RabbitMQConnectionError} from "./core/messaging/errors/RabbitMQConnectionError";
-
-let retries = 25
-
-// delay is in seconds
-const retry = (delay = 5) => {
-    if(retries === 0)
-        return console.error("Maximum retry count exceeded. Stopping.")
-    console.log(`Retrying in ${delay} seconds..`)
-    setTimeout(() => startServer(), delay*1000)
-    --retries
-}
+import {attemptRun} from "./core/attemptRun/attemptRun";
+import {DBConnectionError} from "./core/database/errors/DBConnectionError";
 
 export const startServer = async () => {
-    try {
-        let app = await getApp()
-        app.listen( app.get('port'), _ => console.log('App running on port ' + app.get('port')))
-    } catch (e) {
-        if( e instanceof RabbitMQConnectionError) {
-            console.error("Could not connect to RabbitMQ", e)
-            return retry()
-        }
-        console.error(e)
-        throw new Error("Failed to start server. Unknown reason")
-    }
+    let app = await getApp()
+    app.listen( app.get('port'), _ => console.log('App running on port ' + app.get('port')))
 }
 
-startServer()
+attemptRun({
+    func: startServer,
+    delayBetweenRetries: 5000,
+    maxAttempts: 20,
+    whitelistErrors: [
+        DBConnectionError,
+        RabbitMQConnectionError
+    ]
+})
